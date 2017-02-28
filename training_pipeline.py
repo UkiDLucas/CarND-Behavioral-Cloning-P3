bf966c0c31e0780a9d823182175965aee233bab6
Uki D. Lucas <UkiDLucas@gmail.com>
2017-02-28 08:32:36 -0600

    Create a list of image paths

--- training_pipeline.py
+++ training_pipeline.py
@@ -9,9 +9,9 @@
 
 # In[1]:
 
-data_dir = "../_DATA/CarND/p3_behavioral_cloning/r_001"
-image_dir = "IMG"
-driving_data_csv = "driving_log.csv"
+data_dir = "../_DATA/CarND/p3_behavioral_cloning/r_001/"
+image_dir = "IMG/"
+driving_data_csv = "driving_log_normalized.csv"
 model_dir = "../_DATA/MODELS/"
 model_name = "model_p3_14x64x3_"
 batch_size = 256
@@ -51,7 +51,7 @@ headers, data = read_csv(data_dir + driving_data_csv)
 
 # # Split data into training, testing and validation sets
 
-# In[ ]:
+# In[4]:
 
 from DataHelper import split_random
 training, testing, validation = split_random(data, percent_train=75, percent_test=15) 
@@ -63,11 +63,11 @@ print("validation", validation.shape)
 
 # # Fetch and visualize training steering angles
 # 
-# I would like to train a car on the set that has a nice bell cureve distribution of values:
+# I would like to train a car on the set that has a nice bell curve distribution of values:
 # - I can drive the car on the track backwards
 # - I can flip each image (and value)
 
-# In[ ]:
+# In[5]:
 
 from DataHelper import plot_histogram, get_steering_values, find_nearest
 steering_angles = get_steering_values(training)
@@ -76,7 +76,7 @@ plot_histogram("steering values", steering_angles, change_step=0.01)
 
 # # Extract image names
 
-# In[ ]:
+# In[6]:
 
 from DataHelper import get_image_center_values 
 image_names = get_image_center_values(training)
@@ -86,7 +86,7 @@ print(image_names[1])
 
 # # Create a list of image paths
 
-# In[ ]:
+# In[7]:
 
 image_paths = []
 for image_name in image_names:
