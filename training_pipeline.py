72f2133a00ccf71ce8ca2c1621013ae34d3f06cb
Uki D. Lucas <UkiDLucas@gmail.com>
2017-03-04 13:52:57 -0600

    training the model

--- training_pipeline.py
+++ training_pipeline.py
@@ -89,7 +89,7 @@ print(image_names[1])
 # In[7]:
 
 image_paths = []
-for image_name in image_names:
+for image_name in image_names: # [0:50]
     image_paths.extend([data_dir + image_dir + image_name])
 print(image_paths[1]) 
 
@@ -101,9 +101,14 @@ print(image_paths[1])
 
 # In[8]:
 
-#import numpy as np 
+import numpy as np 
 from ImageHelper import read_image_array
-training_features = [read_image_array(path) for path in image_paths]
+#training_features = [read_image_array(path) for path in image_paths]
+
+image_list = []
+for path in image_paths:
+    image_list.append(read_image_array(path))
+training_features = np.array(image_list) # numpy array, not just a list
 
 print ("training_features count", len(training_features) )
 
@@ -121,7 +126,7 @@ plt.show()
 # 
 # https://keras.io/layers/convolutional/
 
-# In[12]:
+# In[9]:
 
 import keras.backend as K
 from keras.models import Sequential
@@ -136,17 +141,20 @@ from DataHelper import mean_pred, false_rates
 from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D, Convolution1D
 
 
-# In[14]:
+# In[10]:
 
 # Build a Convolutional Neural Network
 
 
-# In[13]:
+# In[11]:
+
+# Expected to see 1 arrays but instead got the following list of 8792 arrays: [array([[[203, 156, 129],
+
 
 model = Sequential()
-
+# sample_image   (160, 320, 3)
 model.add(Convolution2D(64, 3, 3, border_mode='same', activation="relu" ,
-                        input_shape=(14, 64 ,3), dim_ordering='tf', name="conv2d_1_relu"))
+                        input_shape=(160, 320 ,3), dim_ordering='tf', name="conv2d_1_relu"))
 model.add(Convolution2D(64, 3, 3, border_mode='same', activation="relu", name="conv2d_2_relu" ))
 model.add(Convolution2D(64, 5, 5, border_mode='same', activation="relu", name="conv2d_3_relu" ))
 
@@ -162,9 +170,52 @@ model.add(Dense(256, activation="relu", name="dense_2_relu" )) #256
 model.add(Dense(41, activation='linear' , name="dense_3_41_linear")) # default: linear | softmax | relu | sigmoid
 
 # REGRESSION
-#model.add(Dense(1, activation='linear'))
+model.add(Dense(1, activation='linear'))
 
 model.summary()
+
+
+# # Compile model (configure learning process)
+
+# In[ ]:
+
+# Before training a model, you need to configure the learning process, which is done via the compile method.
+# 
+# keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+
+optimizer='sgd' # | 'rmsprop'
+loss_function="mse" # | 'binary_crossentropy' | 'mse' | mean_squared_error | sparse_categorical_crossentropy
+metrics_array=['accuracy'] # , mean_pred, false_rates
+
+model.compile(optimizer, loss_function, metrics_array)
+
+
+# # Replace model with one stored on disk
+# 
+# - If you replace the model, the INPUT dimetions have to be the same as these trained
+# - Name your models well
+from keras.models import load_model
+
+if should_retrain_existing_model:
+    model_path = model_dir + model_to_continue_training
+    model = load_model(model_path) 
+    model.summary()
+# # Train (fit) the model agaist given labels
+
+# In[ ]:
+
+print( "training_features.shape", len(training_features) )
+# REGRESSION
+history = model.fit(training_features, 
+                    y = steering_angles, 
+                    nb_epoch = nb_epoch, 
+                    batch_size = batch_size, 
+                    verbose = 2, 
+                    validation_split = 0.2)
+
+# CLASSIFICATION
+#history = model.fit(training_features, 
+#y_one_hot, nb_epoch=nb_epoch, batch_size=batch_size, verbose=1, validation_split=0.2)
 
 
 # In[ ]:
